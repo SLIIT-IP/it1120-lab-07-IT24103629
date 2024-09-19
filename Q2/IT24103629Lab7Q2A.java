47bc0a94919186c41d6ec9c39e560a68dd254ebf
VIRUL METHDINU MEEMANA <it24103629@my.sliit.lk>
2024-09-19 11:23:39 +0530

    upload IT24103629Lab7Q2

---
+++ Q2/IT24103629Lab7Q2A.java
@@ -0,0 +1,21 @@
+public class IT24103629Lab7Q1A
+{
+	public static void main(String[] args)
+	
+	{
+		
+	int row = 4, collum = 5;
+	
+	for(int i = 0; i<row; i++)
+	{
+		
+		for(int j = 0; j<collum; j++)
+		{
+			System.out.print("$");
+		}
+		System.out.println();
+		
+	}
+
+	}
+}
---
+++ Q2/IT24103629Lab7Q2C.java
@@ -0,0 +1,16 @@
+public class IT24103629Lab7Q2C
+    public static void main(String[] args) {
+        
+        for (int i = 5; i <= 5; i--) 
+        {
+          
+           
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
+            }
+            
+       
+            System.out.println();
+        }
+    }
+}
